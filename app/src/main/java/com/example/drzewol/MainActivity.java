3d2a12cf2e11628190aa20a6d1b02f31b3b20240
misiekroz <misiekroz@gmail.com>
2019-10-15 16:20:47 +0200

    added mandatory public variables for data passing between functions

--- app/src/main/java/com/example/drzewol/MainActivity.java
+++ app/src/main/java/com/example/drzewol/MainActivity.java
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     Button zglos, listaa, mapa;
     Animation choinka_z_prawej, napis_z_lewej;
 
+    public String Title = "", Description = "";
+    public double Lat = 0.0000, Long = 0.0000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
