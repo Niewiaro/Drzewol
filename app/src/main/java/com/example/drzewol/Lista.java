05db6f6e284bc292845fc77134c78c3c58ec3bfb
Niewiaro <kubuskrol@gmail.com>
2019-10-16 01:23:17 +0200

    Dzban MACIEK WEZ SIE DO ROBOTY!!!11!!!!!111!

--- app/src/main/java/com/example/drzewol/Lista.java
+++ app/src/main/java/com/example/drzewol/Lista.java
@@ -3,12 +3,41 @@ package com.example.drzewol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Lista extends AppCompatActivity {
+
+    Button button;
+    ImageView dzban;
+    Animation choinka_z_prawej;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        dzban = (ImageView) findViewById(R.id.dzban);
+        button = (Button) findViewById(R.id.spin);
+        textView = (TextView) findViewById(R.id.XDDD);
+
+        choinka_z_prawej = AnimationUtils.loadAnimation(this,R.anim.choinka_z_prawej);
+
+        dzban.setAnimation(choinka_z_prawej);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText ("↑\nJESTEŚ\nI FRAJER!!!");
+            }
+        });
+
+
     }
 }
