47b7d5f2c7a092da223600ebc2866ff514bd82c1
Niewiaro <kubuskrol@gmail.com>
2019-10-15 18:24:59 +0200

    Apka oprawa graficzna guzik funkcja zglos (work in progress)

--- app/src/main/java/com/example/drzewol/Zglos.java
+++ app/src/main/java/com/example/drzewol/Zglos.java
@@ -2,7 +2,12 @@ package com.example.drzewol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
 
 public class Zglos extends AppCompatActivity {
 
