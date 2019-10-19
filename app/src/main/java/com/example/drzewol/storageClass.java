90f2b152464e4f524c9bae9d0a2c597fa635b2b7
Niewiaro <kubuskrol@gmail.com>
2019-10-20 00:20:10 +0200

    storageClass pierwsze podejscie

---
+++ app/src/main/java/com/example/drzewol/storageClass.java
@@ -0,0 +1,16 @@
+package com.example.drzewol;
+
+public class storageClass {
+
+    private static final String ILE = "ile.txt";
+//pojebane to nie dam rady teraz :(
+    public static void main(String[] args){
+
+        int arrInt[] = new int[1000];
+
+        int i = 1;
+        while (i<=4){
+            arrInt[i]
+        }
+    }
+}
