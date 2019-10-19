c52e33cfae03f8590822376487f43a43a7999595
012323123 <012323123@protonmail.com>
2019-10-19 20:57:38 +0200

    da sie dostac do panelu aparat

--- app/src/main/java/com/example/drzewol/Aparat.java
+++ app/src/main/java/com/example/drzewol/Aparat.java
@@ -2,13 +2,25 @@ package com.example.drzewol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Aparat extends AppCompatActivity {
+
+    Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aparat);
+
+        submitBtn = findViewById(R.id.SubmitBtn);
+    }
+
+    public void openMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
