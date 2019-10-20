db7877562695c8d4483ef689d0ab3e551fd4bf7a
Niewiaro <kubuskrol@gmail.com>
2019-10-20 10:46:07 +0200

    storageClass nie dziala :(

--- app/src/main/java/com/example/drzewol/storageClass.java
+++ app/src/main/java/com/example/drzewol/storageClass.java
@@ -1,16 +1,35 @@
 package com.example.drzewol;
 
+import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class storageClass {
 
-    private static final String ILE = "ile.txt";
-//pojebane to nie dam rady teraz :(
+/*    private static final String ILE = "ile.txt";
+
+    if(ILE = null to znaczy, ze jeszcze nie ma zgloszen)
+
     public static void main(String[] args){
 
         int arrInt[] = new int[1000];
 
         int i = 1;
-        while (i<=4){
-            arrInt[i]
-        }
+       // while (i<=4){
+           // arrInt[i]
+       // }
     }
+
+    public void save(View v) {
+        FileOutputStream fos = null;
+
+        try {
+            fos =
+        } catch (FileNotFoundException e ) {
+            e.printStackTrace();
+        }
+    }*/
 }
